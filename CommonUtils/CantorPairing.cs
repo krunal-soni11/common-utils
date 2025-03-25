2bf39bd80f95ced75393bb9138ad45a96bf0068c
Krunal <krunal.soni11@outlook.com>
2025-03-25 19:13:08 +0530

    pairing program added

---
+++ CommonUtils/CantorPairing.cs
@@ -0,0 +1,30 @@
+﻿namespace Common.Utils;
+
+public class CantorPairing
+{
+    //Encoding pairs uniquely for theoretical use 0rder specifix
+    public static int Pair(int x, int y)
+    {
+        return (x + y) * (x + y + 1) / 2 + y;
+    }
+
+    // Cantor inverse function to get (x, y) back from z
+    public static (int x, int y) Unpair(int z)
+    {
+        int w = (int)((Math.Sqrt(8 * z + 1) - 1) / 2);
+        int t = (w * (w + 1)) / 2;
+        int y = z - t;
+        int x = w - y;
+        return (x, y);
+    }
+
+    //static void Main()
+    //{
+    //    int x = 5, y = 3;
+    //    int paired = Pair(x, y);
+    //    Console.WriteLine($"Paired value of ({x}, {y}): {paired}");
+
+    //    var (xDecoded, yDecoded) = Unpair(paired);
+    //    Console.WriteLine($"Decoded values: ({xDecoded}, {yDecoded})");
+    //}
+}
---
+++ CommonUtils/SzudzikPairing.cs
@@ -0,0 +1,29 @@
+﻿namespace Common.Utils;
+
+public class SzudzikPairing
+{
+    //for a more space-efficient alternative
+    public static int Pair(int x, int y)
+    {
+        return (x >= y) ? (x * x + y) : (y * y + x);
+    }
+
+    // Szudzik inverse function
+    public static (int x, int y) Unpair(int z)
+    {
+        int w = (int)Math.Floor(Math.Sqrt(z));
+        int t = z - (w * w);
+
+        return (t < w) ? (t, w) : (w, t);
+    }
+
+    //static void Main()
+    //{
+    //    int x = 5, y = 3;
+    //    int paired = Pair(x, y);
+    //    Console.WriteLine($"Paired value of ({x}, {y}): {paired}");
+
+    //    var (xDecoded, yDecoded) = Unpair(paired);
+    //    Console.WriteLine($"Decoded values: ({xDecoded}, {yDecoded})");
+    //}
+}
